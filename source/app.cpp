756a7adb41848647af625db8497caf39305c4478
yorung <yorung@gmail.com>
2013-12-11 23:12:20 +0900

    tiny branch:)

--- source/app.cpp
+++ source/app.cpp
@@ -10,7 +10,7 @@ App::~App()
 
 void App::Init()
 {
-	mesh = new MeshX(".\\jiji.x");
+	mesh = new MeshX("C:\\Program Files (x86)\\Microsoft DirectX SDK (August 2009)\\Samples\\Media\\Tiny\\tiny.x");
 
 	matrixMan.Set(MatrixMan::PROJ, XMMatrixPerspectiveFovLH(45 * XM_PI / 180, (float)SCR_W / SCR_H, 0.1f, 1000.0f));
 }
@@ -27,7 +27,7 @@ void App::Draw()
 //	matrixMan.Set(MatrixMan::VIEW, XMMatrixLookAtLH(XMVectorSet(5, 5, 5, 1), XMVectorSet(0, 0, 0, 0), XMVectorSet(0, 1, 0, 0)));
 	matrixMan.Set(MatrixMan::WORLD, XMMatrixRotationQuaternion(XMQuaternionRotationAxis(XMVectorSet(1, 0, 0, 0), time / 2 * XM_PI)));
 
-	float dist = 5;
+	float dist = 400;
 	float rot = time / 5 * XM_PI;
 	matrixMan.Set(MatrixMan::VIEW, XMMatrixLookAtLH(XMVectorSet(sin(rot) * dist, 0, cos(rot) * dist, 1), XMVectorSet(0, 0, 0, 0), XMVectorSet(0, 1, 0, 0)));
 
